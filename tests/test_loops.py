78438cc3f68c2100d6b2ef6a0d04e153bef2635b
StarrFox <StarrFox6312@gmail.com>
2023-12-03 10:55:02 +0000

    only define fixtures once

--- tests/test_loops.py
+++ tests/test_loops.py
@@ -1,25 +1,3 @@
-from collections.abc import Callable
-from typing import Any
-
-import pytest
-
-from serqlane.astcompiler import ModuleGraph
-from serqlane.vm import SerqVM
-
-
-@pytest.fixture
-def checking_executor() -> Callable[[str, str, Any], None]:
-    def execute_with_check(code: str, variable: str, value: Any):
-        graph = ModuleGraph()
-        vm = SerqVM()
-        module = graph.load("<string>", code)
-        vm.execute_module(module)
-
-        assert vm.get_stack_value_by_name(variable) == value
-
-    return execute_with_check
-
-
 def test_while(checking_executor):
     checking_executor("""
 let mut x = 10
