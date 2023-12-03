f77a82814ed76c6adbe0b3c4674d1ecce4a6a9d2
SirOlaf <.>
2023-12-03 16:34:07 +0100

    Fix returns

--- serqlane/astcompiler.py
+++ serqlane/astcompiler.py
@@ -937,11 +937,12 @@ class CompCtx(lark.visitors.Interpreter):
 
         if len(body_node.children) > 0:
             last_body_node = body_node.children[-1]
-            if last_body_node.type.kind in literal_types:
+            if last_body_node.type == None or last_body_node.type.kind == TypeKind.unit:
+                pass
+            elif last_body_node.type.kind in literal_types:
                 body_node = self.visit(tree.children[3], ret_type)
-
-            if last_body_node.type.kind in builtin_userspace_types:
-                assert last_body_node.type.types_compatible(ret_type), f"Invalid return expression type {last_body_node.type.render()} for return type {ret_type.render()}"
+                if last_body_node.type.kind in builtin_userspace_types:
+                    assert last_body_node.type.types_compatible(ret_type), f"Invalid return expression type {last_body_node.type.render()} for return type {ret_type.render()}"
 
         self.fn_ret_type_stack.pop()
 
