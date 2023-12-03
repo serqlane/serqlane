0c39505f419368f58fc981532dbbf8900ecd81b4
StarrFox <StarrFox6312@gmail.com>
2023-12-03 06:32:29 +0000

    fix single lines and tests

--- tests/test_binary_operators.py
+++ tests/test_binary_operators.py
@@ -33,21 +33,21 @@ def checking_executor() -> Callable[[str, str, Any], None]:
 
 
 def test_literal_arith(checking_executor: Callable[[str, str, Any], None]):
-    checking_executor("let x = 1 + 1;", "x", 2)
-    checking_executor("let x = 1 - 1;", "x", 0)
-    checking_executor("let x = 1 * 2;", "x", 2)
-    checking_executor("let x = 1 / 1;", "x", 1)
+    checking_executor("let x = 1 + 1", "x", 2)
+    checking_executor("let x = 1 - 1", "x", 0)
+    checking_executor("let x = 1 * 2", "x", 2)
+    checking_executor("let x = 1 / 1", "x", 1)
 
-    checking_executor("let x = 1 + 1 + 1;", "x", 3)
-    checking_executor("let x = 10 / (3 + 2);", "x", 2)
-    checking_executor("let x = (10 / (3 + 2));", "x", 2)
+    checking_executor("let x = 1 + 1 + 1", "x", 3)
+    checking_executor("let x = 10 / (3 + 2)", "x", 2)
+    checking_executor("let x = (10 / (3 + 2))", "x", 2)
 
 
 def test_variable_arith(checking_executor: Callable[[str, str, Any], None]):
     checking_executor(
         """
-let x = 1;
-let y = x + 2;
+let x = 1
+let y = x + 2
 """,
         "y",
         3,
@@ -55,8 +55,8 @@ let y = x + 2;
 
     checking_executor(
         """
-let x = 1;
-let y = x + x;
+let x = 1
+let y = x + x
 """,
         "y",
         2,
@@ -67,23 +67,23 @@ def test_type_inference(executor, checking_executor):
     with pytest.raises(ValueError):
         executor(
             """
-let x = "abc";
-let y = x + 1;
+let x = "abc"
+let y = x + 1
 """
         )
 
     with pytest.raises(ValueError):
         executor("""
-let x: int32 = 10;
-let y: int64 = 20;
-let y: int64 = (x + x) - y;
+let x: int32 = 10
+let y: int64 = 20
+let y: int64 = (x + x) - y
     """)
 
     with pytest.raises(ValueError):
-        executor("let x = 1 == true;")
+        executor("let x = 1 == true")
 
     checking_executor("""
-let x = 100;
-let y = true;
-let z = x > 0 and (x == 100) or false;
+let x = 100
+let y = true
+let z = x > 0 and (x == 100) or false
 """, "z", True)
--- tests/test_types.py
+++ tests/test_types.py
@@ -22,4 +22,4 @@ def executor() -> Callable[[str], SerqVM]:
 def test_invalid_cohersion(executor: Callable[[str], SerqVM]):
     # TODO: this should probably raise a better exception
     with pytest.raises(AssertionError):
-        executor("let x: int = \"abc\";")
+        executor("let x: int = \"abc\"")
