752ac28f85dd89770967e6e2c6b68a6078133a72
SolarWolf-Code <bradsterstrange0@gmail.com>
2023-12-05 18:50:52 +0000

    use reserved keywords list from ast

--- tests/test_reserved_keywords.py
+++ tests/test_reserved_keywords.py
@@ -1,26 +1,12 @@
 import pytest
+from serqlane.astcompiler import RESERVED_KEYWORDS
 
-reserved_keywords = [
-    "and",
-    "break",
-    "continue",
-    "else",
-    "fn",
-    "if",
-    "let",
-    "mut",
-    "not",
-    "or",
-    "return",
-    "while",
-]
-
-@pytest.mark.parametrize("keyword", reserved_keywords)
+@pytest.mark.parametrize("keyword", RESERVED_KEYWORDS)
 def test_let_reserved_keywords(executor, keyword):
     with pytest.raises(ValueError):
         executor(f"let {keyword} = 1")
 
-@pytest.mark.parametrize("keyword", reserved_keywords)
+@pytest.mark.parametrize("keyword", RESERVED_KEYWORDS)
 def test_fn_name_reserved_keywords(executor, keyword):
     with pytest.raises(ValueError):
         code = f"""
@@ -29,7 +15,7 @@ def test_fn_name_reserved_keywords(executor, keyword):
         }}"""
         executor(code)
 
-@pytest.mark.parametrize("keyword", reserved_keywords)
+@pytest.mark.parametrize("keyword", RESERVED_KEYWORDS)
 def test_fn_args_reserved_keywords(executor, keyword):
     with pytest.raises(ValueError):
         code = f"""
