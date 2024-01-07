22f5e4d616b727f0c6d0c3e306589ce7df7b9ce1
StarrFox <StarrFox6312@gmail.com>
2024-01-07 05:32:49 +0000

    add from import tests

--- tests/test_imports.py
+++ tests/test_imports.py
@@ -20,7 +20,43 @@ pub fn change_x(value: int64) -> int64 {
 }
 """},
         20
-    )
+    ),
+    ({"main": """
+from math import *
+dbg(add(20, 10))
+""", "math": """
+pub fn add(a: int64, b: int64) -> int64 {
+    a + b
+}
+"""}, 30),
+    ({"main": """
+from math import *
+dbg(math.add(20, 20))
+""", "math": """
+pub fn add(a: int64, b: int64) -> int64 {
+    a + b
+}
+"""}, 40),
+    ({"main": """
+from math import [add]
+dbg(add(25, 25))
+""", "math": """
+pub fn add(a: int64, b: int64) -> int64 {
+    a + b
+}
+"""}, 50),
+    ({"main": """
+from math import [add, sub]
+dbg(add(30, sub(40, 10)))
+""", "math": """
+pub fn add(a: int64, b: int64) -> int64 {
+    a + b
+}
+
+pub fn sub(a: int64, b: int64) -> int64 {
+    a - b
+}
+"""}, 60),
 ]
 
 
@@ -29,24 +65,6 @@ def test_imports_passing(multimodule_capture_first_debug, modules, expected):
     assert multimodule_capture_first_debug(modules) == expected
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+# for some reason this is allowed
+def test_no_op_import(multimodule_executor):
+    multimodule_executor({"main": "from other import []", "other": ""})
