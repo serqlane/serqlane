67e8c660a0bdb2fe1299eb88a94b09a8fcfe603d
SirOlaf <sirolaf@protonmail.com>
2024-01-12 02:05:11 +0100

    Iterator, manual inlining

--- serqlane/tokenizer.py
+++ serqlane/tokenizer.py
@@ -2,7 +2,7 @@ import pathlib
 
 from dataclasses import dataclass
 from enum import Enum
-from typing import Callable, Optional
+from typing import Callable, Optional, Iterator
 
 from serqlane.common import SerqInternalError
 
@@ -15,6 +15,7 @@ class TokenizerError(Exception): ...
 class TokenKind(Enum):
     ERROR = "<error>"
     NEWLINE = "<newline>"
+    EOF = "<eof>"
 
     # symbols
     PLUS = "+"
@@ -71,13 +72,27 @@ class TokenKind(Enum):
     STRING = "{string}"
     SINGLE_LINE_DOC_COMMENT = "{doc}"
 
+    def is_keyword(self) -> bool:
+        return not self.is_symbolic() and self.name.isalnum()
+
     def is_symbolic(self) -> bool:
-        if self in [TokenKind.ERROR, TokenKind.NEWLINE]:
+        if self in [TokenKind.ERROR, TokenKind.NEWLINE, TokenKind.EOF]:
             return False
         return not self.value[0].isalnum() and not (self.value.startswith("{") and not self.value.startswith("}"))
 
-MULTICHAR_MARKERS = set(
+KEYWORDS = dict(
+    (x.value, x) for x in TokenKind if x.is_keyword()
+)
+
+# Also contains () and similar
+SINGLECHAR_OPERATORS = dict(
+    (x.value, x) for x in TokenKind if len(x.value) == 1 and x.is_symbolic()
+)
+MULTICHAR_MARKERS = frozenset(
     x.value[0] for x in TokenKind if len(x.value) > 1 and x.is_symbolic()
+)
+MULTICHAR_OPERATORS = dict(
+    (x.value, x) for x in TokenKind if len(x.value) > 1 and x.is_symbolic()
 )
 for t in TokenKind:
     if t.is_symbolic():
@@ -99,11 +114,20 @@ class LineInfo:
 
 
 class Token:
-    def __init__(self, kind: TokenKind, literal: str, line_info: LineInfo, *, value=None) -> None:
+    def __init__(self, kind: TokenKind, literal: str, *, line: int, column_start: int, column_end: int) -> None:
         self.kind = kind
         self.literal = literal
-        self.line_info = line_info
-        self.value = value
+
+        self.line = line
+        self.column_start = column_start
+        self.column_end = column_end
+
+    def get_line_info(self) -> LineInfo:
+        return LineInfo(
+            self.line,
+            self.column_start,
+            self.column_end,
+        )
  
     def render(self) -> str:
         val = self.kind.value
@@ -127,18 +151,17 @@ class Token:
 
 class Tokenizer:
     def __init__(self, data: str, *, filepath: Optional[pathlib.Path] = None) -> None:
-        self.data = data
+        self.data = data.strip()
         self.offset = 0
         self.line = 0
         self.column = 0
         self.filepath = filepath
-
-    def remaining(self) -> int:
-        return len(self.data) - self.offset
+        self.remaining = len(self.data)
 
     def advance(self) -> None:
         c = self.data[self.offset]
         self.offset += 1
+        self.remaining -= 1
         if c == "\n":
             self.line += 1
             self.column = 0
@@ -146,7 +169,7 @@ class Tokenizer:
             self.column += 1
 
     def peek(self, offset=1) -> str:
-        if self.remaining() > offset:
+        if self.remaining > offset:
             return self.data[self.offset + offset]
         return ""
     
@@ -156,7 +179,7 @@ class Tokenizer:
             return ""
         result = c
         self.advance()
-        while self.remaining() > 0:
+        while self.remaining > 0:
             c = self.peek(0)
             if not pred(c):
                 break
@@ -164,47 +187,88 @@ class Tokenizer:
             self.advance()
         return result
 
-    def make_token(self, kind: TokenKind, literal: str, *, value=None) -> Token:
+    def make_token(self, kind: TokenKind, literal: str) -> Token:
         return Token(
             kind=kind,
             literal=literal,
-            line_info=LineInfo(
-                self.line,
-                self.column - len(literal),
-                self.column,
-            ),
-            value=value,
+            line=self.line,
+            column_start=self.column - len(literal),
+            column_end=self.column,
         )
 
     def report_error(self, message: str, tok: Token):
         if self.filepath != None:
-            location_str = f"\nAt: {self.filepath.absolute()}:{tok.line_info.to_path_cursor()}"
+            location_str = f"\nAt: {self.filepath.absolute()}:{tok.get_line_info().to_path_cursor()}"
         else:
-            location_str = f"\nline: {tok.line_info.line}\ncolumn: {tok.line_info.column_start}"
+            location_str = f"\nline: {tok.get_line_info().line}\ncolumn: {tok.get_line_info().column_start}"
         
         message += location_str
         raise TokenizerError(message)
 
-    def put_newline(self, result: list[Token]):
-        if len(result) == 0 or result[-1].kind == TokenKind.NEWLINE:
-            return
-        result.append(self.make_token(TokenKind.NEWLINE, "\n"))
-
-    def process(self) -> list[Token]:
-        result: list[Token] = []
-
-        while True:
+    def process_iter(self) -> Iterator[Token]:
+        while self.remaining > 0:
             c = self.peek(0)
-            assert len(c) == 1
 
-            if c.isnumeric():
+            if c.isalpha():
+                # identifier or keyword
+                # manually inlined take_while
+                ident = c
+                self.advance()
+                while self.remaining > 0:
+                    cc = self.peek(0)
+                    if not (cc.isalnum() or cc == "_"):
+                        break
+                    ident += cc
+                    self.advance()
+                kind = KEYWORDS.get(ident, TokenKind.IDENTIFIER)
+                yield Token(
+                    kind=kind,
+                    literal=ident,
+                    line=self.line,
+                    column_start=self.column - len(ident),
+                    column_end=self.column,
+                )
+            elif c.isspace():
+                # space; advance keeps track of line and column already
+                if c == "\n":
+                    p = self.peek(1)
+                    if p != "\n" and not p.isspace():
+                        yield Token(
+                            kind=TokenKind.NEWLINE,
+                            literal=c,
+                            line=self.line,
+                            column_start=self.column - len(c),
+                            column_end=self.column,
+                        )
+                self.advance()
+            elif c.isnumeric():
                 # int or float
-                lit = self.take_while(lambda x: x.isnumeric() or x == ".")
+                # manually inlined take_while
+                lit = c
+                self.advance()
+                while self.remaining > 0:
+                    cc = self.peek(0)
+                    if not (cc.isnumeric() or cc == "."):
+                        break
+                    lit += cc
+                    self.advance()
                 dot_count = lit.count(".")
                 if dot_count == 0:
-                    result.append(self.make_token(TokenKind.INTEGER, lit, value=int(lit)))
+                    yield Token(
+                        kind=TokenKind.INTEGER,
+                        literal=lit,
+                        line=self.line,
+                        column_start=self.column - len(lit),
+                        column_end=self.column,
+                    )
                 elif dot_count == 1:
-                    result.append(self.make_token(TokenKind.DECIMAL, lit, value=float(lit)))
+                    yield Token(
+                        kind=TokenKind.DECIMAL,
+                        literal=lit,
+                        line=self.line,
+                        column_start=self.column - len(lit),
+                        column_end=self.column,
+                    )
                 else:
                     self.report_error(
                         f"Numeric literal with {dot_count} dots found: {lit}",
@@ -231,44 +295,51 @@ class Tokenizer:
                         f"Encountered an unclosed string literal: {lit}",
                         self.make_token(TokenKind.ERROR, lit)
                     )
-                result.append(self.make_token(TokenKind.STRING, lit, value=lit[1:-1]))
-            elif c.isspace():
-                # space; advance keeps track of line and column already
-                if c == "\n":
-                    self.put_newline(result)
-                self.advance()
-            elif c.isalpha():
-                # identifier or keyword
-                ident = self.take_while(lambda x: x.isalnum() or x == "_")
-                kind = TokenKind(ident) if ident in TokenKind else TokenKind.IDENTIFIER
-                result.append(self.make_token(kind, ident))
+                yield Token(
+                    kind=TokenKind.STRING,
+                    literal=lit,
+                    line=self.line,
+                    column_start=self.column - len(lit),
+                    column_end=self.column,
+                )
             else:
                 # symbol or comment
                 if c == "/" and self.peek(1) == "/":
                     # comment
                     com = self.take_while(lambda x: x != "\n")
                     if com.startswith("///"):
-                        result.append(self.make_token(
+                        yield self.make_token(
                             TokenKind.SINGLE_LINE_DOC_COMMENT,
                             com,
-                            value=com[3:].strip()
-                        ))
+                        )
                 else:
                     # symbol
-                    full = c
+                    put_multichar = False
                     if c in MULTICHAR_MARKERS:
                         tmp = c + self.peek(1)
-                        if len(tmp) == 2 and tmp in TokenKind:
-                            full = tmp
+                        multichar_op = MULTICHAR_OPERATORS.get(tmp, None)
+                        if multichar_op != None:
+                            yield Token(
+                                kind=multichar_op,
+                                literal=tmp,
+                                line=self.line,
+                                column_start=self.column - len(tmp),
+                                column_end=self.column,
+                            )
                             self.advance()
-                    if full not in TokenKind:
-                        self.report_error(f"Unrecognized symbol: '{full}'", self.make_token(TokenKind.ERROR, full))
-                    result.append(self.make_token(TokenKind(full), full))
+                            put_multichar = True
+                    if not put_multichar:
+                        if c not in SINGLECHAR_OPERATORS:
+                            self.report_error(f"Unrecognized symbol: '{c}'", self.make_token(TokenKind.ERROR, c))
+                        yield Token(
+                            kind=SINGLECHAR_OPERATORS[c],
+                            literal=c,
+                            line=self.line,
+                            column_start=self.column - len(c),
+                            column_end=self.column,
+                        )
                     self.advance()
+        yield self.make_token(TokenKind.EOF, "")
 
-            if self.remaining() <= 0:
-                break
-
-        if result[-1].kind == TokenKind.NEWLINE:
-            result.pop()
-        return result
+    def process(self) -> list[Token]:
+        return list(self.process_iter())
