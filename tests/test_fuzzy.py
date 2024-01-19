88e042881e1388e1a8788cf379457d025cf55304
StarrFox <StarrFox6312@gmail.com>
2024-01-19 01:19:03 +0000

    make hypothesis optional

--- tests/test_fuzzy.py
+++ tests/test_fuzzy.py
@@ -1,7 +1,4 @@
-import hypothesis
 import pytest
-from hypothesis.extra.lark import from_lark
-from lark import Lark
 from contextlib import suppress
 
 from serqlane.astcompiler import ModuleGraph, SerqTypeInferError
@@ -9,20 +6,25 @@ from serqlane.common import SerqInternalError
 from serqlane.parser import ParserError
 
 
-with open("serqlane/grammar.lark") as fp:
-    grammar = fp.read()
+try:
+    from lark import Lark
+    import hypothesis
+    from hypothesis.extra.lark import from_lark
+except ImportError:
+    pass
+else:
+    with open("serqlane/grammar.lark") as fp:
+        grammar = fp.read()
 
+    lark_parser = Lark(grammar)
 
-lark_parser = Lark(grammar)
+    @pytest.mark.fuzz
+    @hypothesis.settings()
+    @hypothesis.given(
+        from_lark(lark_parser)
+    )
+    def test_fuzz_compiler(code: str):
+        graph = ModuleGraph()
 
-
-@pytest.mark.fuzz
-@hypothesis.settings()
-@hypothesis.given(
-    from_lark(lark_parser)
-)
-def test_fuzz_compiler(code: str):
-    graph = ModuleGraph()
-
-    with suppress(ValueError, AssertionError, ParserError, SerqInternalError, SerqTypeInferError):
-        graph.load("<string>", code)
+        with suppress(ValueError, AssertionError, ParserError, SerqInternalError, SerqTypeInferError):
+            graph.load("<string>", code)
